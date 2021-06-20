06a98b99ec39d1e885172bd94ac2d6c9ed72853f
liliceobr <alice.obr@gmail.com>
2021-06-20 22:27:21 +0200

    Add files via upload

--- a2/mainVoronoi.cpp
+++ a2/mainVoronoi.cpp
@@ -40,17 +40,20 @@ std::vector<Vector> tutte(std::vector<Vector> points, std::vector<std::vector<in
 }
 
 int main(int argc, char *argv[]){
-    int N = 10000;
+    //srand(0);
+
+    int N = 100;
     std::vector<Vector> points(N);
     std::vector<double> lambdas(N);
     Vector middle = Vector(0.5, 0.5, 0);
     double sigma = 0.1;
     for(int i = 0; i < N; i ++) {
         points[i] = Vector(rand01(), rand01(), 0);
+        //points[i] = Vector(rand01(), rand01(), rand01());
         lambdas[i] = gauss(points[i], middle, sigma);
         points[i][2] = lambdas[i];
     }
     std::vector<Polygon> voronoisDiag = voronoiParallel(points);
     print("Done");
-    save_svg(voronoisDiag, "res10000.svg");
+    save_svg(voronoisDiag, "res100_power.svg");
 }
